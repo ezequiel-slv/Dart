7c0edf86cb82f97d3de213ece8295ee77fdae750
Ezequiel <ezequielsilvalima01@gmail.com>
2026-06-05 16:33:07 -0300

    commit

--- src/core/operadores/aritimeticos/op_aritimeticos.dart
+++ src/core/operadores/aritimeticos/op_aritimeticos.dart
@@ -1,3 +1,10 @@
 void main(){
-  print(40 + 2);
+  print(40 + 2); // adição
+  print('40' + '2'); // concatena os valores
+  print(50 - 8); // subtração
+  print(-(-42)); // altera o valor dependendo do sinal
+  print(6 * 7); // multiplicação
+  print(11/2); // divisão
+  print(11 ~/ 2); // retorna a parte inteira
+  print(11 % 2); //resto da divisão
 }
