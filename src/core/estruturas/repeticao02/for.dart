33f895ad20462dd27fc1befb09c0a117c4cebde4
Ezequiel <ezequielsilvalima01@gmail.com>
2026-07-01 16:32:35 -0300

    commit

--- src/core/estruturas/repeticao02/for.dart
+++ src/core/estruturas/repeticao02/for.dart
@@ -1,6 +1,9 @@
 void main(){
     for01();
     for02();
+    for03();
+    for04();
+    for05();
 }
 
   void for01(){
@@ -35,4 +38,36 @@ void for02(){
       // limita a iteração até 7
       print(i);
   }
+}
+
+void for03(){
+  print("----------");
+
+  forDeFora: // apelido criado para chamar o for de 'i'
+  for(var i = 0; i <= 2; i++){
+    print("for de fora: $i"); // imrpime o valor atual de 'i'
+    for(var j = 0; j <= 2; j++){
+      if(j >= 1 || i == 1) continue forDeFora; // chama o for de fora
+      print("for de dentro: $j"); // imprime o valor atual de 'j'
+    }
+  }
+}
+
+void for04(){
+  print("----------");
+
+  final vogais = ['a', 'e', 'i', 'o', 'u'];
+
+  for(final vogal in vogais){ // direciona onde o vogal vai buscar os valores
+    if(vogal == 'e') continue; // continua a busca ignorando o 'e'
+    if(vogal == 'u') break; // para no 'u', sem adicionar o valor
+
+    print(vogal); // imprime os valores da lista
+  }
+}
+
+void for05(){
+  final vogais = ['a', 'e', 'i', 'o', 'u', 'j'];
+  assert(vogais.length == 5, 'Número de vogais não correspondem!');
+  // cancela o fluxo do programa e lança um aviso
 }
