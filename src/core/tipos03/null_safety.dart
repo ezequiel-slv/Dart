ec9528a4b1aa7898cf1c5602a05d4ffb39dd345c
Ezequiel <ezequielsilvalima01@gmail.com>
2026-07-07 21:13:04 -0300

    commit

---
+++ src/core/tipos03/null_safety.dart
@@ -0,0 +1,9 @@
+void main(){
+  int valor;
+  // print(valor.isEven); // aqui o null safety entra em ação
+  
+  /*
+  O Null Safety informa o erro antes de rodar o código
+  Informando que aquela ação irá causar um erro
+   */
+}
