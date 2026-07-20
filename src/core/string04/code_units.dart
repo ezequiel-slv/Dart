f035a1d8325075705472796e1dc184c8e93177c1
Ezequiel <ezequielsilvalima01@gmail.com>
2026-07-20 20:11:25 -0300

    commit

--- src/core/string04/code_units.dart
+++ src/core/string04/code_units.dart
@@ -1,4 +1,9 @@
 void main(){
+  codeUnits();
+  surrogatePair();
+}
+
+void codeUnits(){
   var a = '42';
 
   print(a);
@@ -8,4 +13,16 @@ void main(){
   /* O códigp  acima exibe o número referente ao código 52
      com a concatenação do código 50
    */
+}
+
+void surrogatePair(){
+  print("----------");
+  var  spock = '\u{1f596}'; // código do emoji
+
+  print(spock);
+  print(spock.length); // o código foi quebrado em duas partes de 16 bits
+  print(spock.codeUnits); // exibe os codigos do UTF-16
+  print(spock.runes); // número de cada letra
+  print(String.fromCharCode(55357) + String.fromCharCode(56726));
+  // acima é montado o emoji a partir dos 2 códigos utf
 }
