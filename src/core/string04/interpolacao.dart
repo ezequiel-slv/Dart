cce59a17a255cc5ada47e9a033ea04a9c3c1a785
Ezequiel <ezequielsilvalima01@gmail.com>
2026-07-21 19:49:31 -0300

    commit

---
+++ src/core/string04/interpolacao.dart
@@ -0,0 +1,8 @@
+int num = 42;
+
+void main() {
+  print("O resultado da multiplicação do número: $num e $num é: ${num * num}");
+  // $: simbolo que faz a interpolação da variável, podendo acessar utiliizando o cifrão
+  print("Variável: \$num"); // imprime tudo depois da \, inclusive o cifrão
+  print(r'Variável: $num'); // imprime tudo depois do r 
+}
