eb3dd01b870420f2ea53c62778cd78f3a878e685
Ezequiel <ezequielsilvalima01@gmail.com>
2026-09-04 21:02:49 -0300

    commit

---
+++ src/core/jogo/lib/src/partida.dart
@@ -0,0 +1,7 @@
+const pedra = 'Pedra';
+const papel = 'Papel';
+const tesoura = 'Tesoura';
+const lagarto = 'Lagarto';
+const spock = 'Spock';
+
+const opcoes = [pedra, papel, tesoura, lagarto, spock];
